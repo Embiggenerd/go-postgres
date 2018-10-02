ca17a08f2c9583b06c22f6129a562d727d0d6138
igor atakhanov <igoratakhanov@gmail.com>
2018-10-02 13:35:30 -0400

    added some database skeleton. TODO: crud routes -> templates -> db

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// create http routes for adding, deleting, changing todos
+// create user, todos model
+// [form data --> db, db --> templates]
+// incorporate sessions, login, register
+// [auth middleware, data validation]
+// learn testing along the way
+
+import (
+	_ "github.com/lib/pq"
+)
+
+func main() {
+
+}
---
+++ models/database.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "go"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
+		host, port, user, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	type User struct {
+		ID        int
+		Age       int
+		FirstName string
+		LastName  string
+		Email     string
+	}
+
+	sqlStatement := `SELECT * FROM users WHERE id=$1`
+	var user User
+
+	row := db.QueryRow(sqlStatement, 2)
+	err = row.Scan(&user.ID, &user.Age, &user.FirstName, &user.LastName, &user.Email)
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned")
+		return
+	case nil:
+		fmt.Println(user)
+	default:
+		panic(err)
+	}
+}
